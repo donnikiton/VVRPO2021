4a8aa8e68ab1a75ef51ca400269e40f5e5eda77e
donnikiton <94794375+donnikiton@users.noreply.github.com>
2022-01-17 09:55:58 +0300

    Add files via upload

---
+++ TestOne/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestOne")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestOne")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0508fe7a-5953-49e2-85e8-72c2c60d9bbd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestOne/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestOne
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+            Public void AllSumInMonth()
+            {
+                value = bilet.SumInMonth(150, 30)
+                Assert.AreEqual(4500, value);
+            }
+      
+    }
+}
